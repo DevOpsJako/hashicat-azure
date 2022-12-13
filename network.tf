9de7224dc2e468d316a7fffcea8947f8ede487c1
JT <you@example.com>
2022-12-13 10:52:02 +0000

    Added network module again

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/jadrankotomic/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "gaurav"
+  resource_group_name = "jadrankotom-workshop"
 }
